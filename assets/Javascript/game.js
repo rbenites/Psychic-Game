c3d256f1000b474356617f6cb58fd621e67cfa67
rbenites <rbenites115@gmail.com>
2018-03-14 22:49:15 -0700

    moved the HTML file out of the assets folder and updated source location

--- assets/Javascript/game.js
+++ assets/Javascript/game.js
@@ -4,31 +4,38 @@ function hold() {
     document.getElementById('wins').innerHTML = wins;
     var losses = 0;
     document.getElementById('losses').innerHTML = losses;
-   // var playerGuess;
-   // var playerGuesses = [];
+    // var playerGuess;
+    // var playerGuesses = [];
     var guessesLeft = 15;
     document.getElementById('guessesLeft').innerHTML = guessesLeft;
 
 
-//This will hold an array of letters (the Alphabet)
-var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    //This will hold an array of letters (the Alphabet)
+    var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
 
 /*
-var difficultyLevel = ["Easy", "Medium", "Hard"];
-for (var i = 0; i < difficultyLevels.length; i++) {
-    var button = document.createElement('button');
-    button.innerHTML = difficultyLevels[i];
-    button.setAttribute('level', difficultyLevels[i]);
-    document.getElementById('game').append(button);
-    button.onclick = function () {
-        playGame(this.getAttribute('level'));
-    };
-}
-*/
+    //this Jquery alerts when the radial buttons are changed
+    $('#difficultySelector input').on('change', function () {
+        alert($('input[name=inlineRadioOptions]:checked', '#difficultySelector').val());
+    });
+console.log(difficultySelector);
 
-function playGame(/*level*/) {
-    
-    /*
+ /* trying to figure out how to use JS to get the easy/medium/hard values and pass them to a var that will be used in the playGame function
+    function difficultyFunction(difficulty){
+        var easy = document.getElementById("easy");
+        var medium = document.getElementById("medium");
+        var hard = document.getElementById("hard");
+        playGame(this.difficultyFunction("difficulty"));   
+    }
+
+    */
+
+
+
+
+    function playGame(/*level*/) {
+        /*
         if (level === "Easy") {
             guaessesLeft = 15;
         } else if (level === "Medium") {
@@ -36,49 +43,51 @@ function playGame(/*level*/) {
         } else {
             guessesLeft = 5;
         }
-    */
-    //Randomizer for computer to pick a letter
-    var computerpick = letters[Math.floor(Math.random() * letters.length)];
-    console.log(computerpick);
+        console.log(level);
+        */
 
-    var playerGuesses=[];
-    document.getElementById('guessed-letters').innerHTML = playerGuesses;
+        //Randomizer for computer to pick a letter
+        var computerpick = letters[Math.floor(Math.random() * letters.length)];
+        console.log(computerpick);
 
-    document.onkeypress = function (event) {
-        var usersGuessedLetter = event.key;
-        console.log(usersGuessedLetter);
+        var playerGuesses = [];
+        document.getElementById('guessed-letters').innerHTML = playerGuesses;
+
+        document.onkeypress = function (event) {
+            var usersGuessedLetter = event.key;
+            console.log(usersGuessedLetter);
 
 
-        if (playerGuesses.indexOf(usersGuessedLetter) === -1) {
-            guessesLeft--;
-            document.getElementById('guessesLeft').innerHTML = guessesLeft;
-            playerGuesses.push(usersGuessedLetter);
-            document.getElementById('guessed-letters').innerHTML = playerGuesses;
-            if (computerpick === usersGuessedLetter) {
-                wins++;
-                document.getElementById('wins').innerHTML = wins;
-                usersGuessedLetter=[];
-                document.getElementById('guessed-letters').innerHTML = playerGuesses;
-                alert("You are winner HAHAHA");
-                playerGuesses=[];
-                guessesLeft=15;
+            if (playerGuesses.indexOf(usersGuessedLetter) === -1) {
+                guessesLeft--;
                 document.getElementById('guessesLeft').innerHTML = guessesLeft;
-                playGame();
-            } else if (guessesLeft === 0) {
-                losses++;
-                document.getElementById('losses').innerHTML = losses;
-                alert("You lose! Your soul is mine");
-                usersGuessedLetter=[];
+                playerGuesses.push(usersGuessedLetter);
                 document.getElementById('guessed-letters').innerHTML = playerGuesses;
-                playerGuesses=[];                
-                guessesLeft=15;
-                document.getElementById('guessesLeft').innerHTML = guessesLeft;
-                playGame();
+                if (computerpick === usersGuessedLetter) {
+                    wins++;
+                    document.getElementById('wins').innerHTML = wins;
+                    usersGuessedLetter = [];
+                    document.getElementById('guessed-letters').innerHTML = playerGuesses;
+                    alert("You are winner HAHAHA");
+                    playerGuesses = [];
+                    guessesLeft = 15;
+                    document.getElementById('guessesLeft').innerHTML = guessesLeft;
+                    playGame();
+                } else if (guessesLeft === 0) {
+                    losses++;
+                    document.getElementById('losses').innerHTML = losses;
+                    alert("You lose! Your soul is mine");
+                    usersGuessedLetter = [];
+                    document.getElementById('guessed-letters').innerHTML = playerGuesses;
+                    playerGuesses = [];
+                    guessesLeft = 15;
+                    document.getElementById('guessesLeft').innerHTML = guessesLeft;
+                    playGame();
+                }
             }
-        }
-    };
-}
-playGame();
+        };
+    }
+    playGame();
 }
 /*
 
